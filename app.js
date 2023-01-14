5064c2013ea182d6733bbb44e34665242db40f3d
Dima Osenkov <w3rgy1992@gmail.com>
2023-01-14 23:03:03 +0100

    up2

--- app.js
+++ app.js
@@ -20,4 +20,29 @@ function windowLoad() {
 			  },
 		});
 	}
+
+	document.addEventListener("click", documentActions);
+
+	function documentActions(e) {
+		const targetElement = e.target;
+		//Tabs
+		if (targetElement.closest('.nav-popular__item')) {
+			const tabNavItem = targetElement.closest('.nav-popular__item');
+			if (!tabNavItem.classList.contains('active')) {
+				const activeTabNavItem = document.querySelector('.nav-popular__item.active');
+				activeTabNavItem.classList.remove('active');
+				tabNavItem.classList.add('active');
+
+				const tabItems = document.querySelectorAll('.popular__tab');
+				const activeTabItem = document.querySelector('.popular__tab.active');
+
+				activeTabItem.classList.remove('active');
+				tabItems[getIndex(tabNavItem)].classList.add('active');
+			}
+		}
+	}
+
+	function getIndex(el) {
+		return Array.from(el.parentNode.children).indexOf(el);
+	}
 }
